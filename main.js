d3f903e2f6bf8112b55dcf8cc2e855907ed8667a
anant20808 <76085631+anant20808@users.noreply.github.com>
2022-08-13 11:56:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+status1="";
+objectinput="";
 function preload(){
 
 }
 function setup(){
-canvas=createCanvas(300,400);
+canvas=createCanvas(600,500);
 canvas.center();
 video = createCapture(VIDEO);
+video.hide()
 }
 function draw(){
-image(video,0,0,300,400)
+image(video,0,0,600,500)
+}
+function start(){
+objectDetector = ml5.objectDetector('cocossd',modelloaded);
+document.getElementById("statusq").innerHTML="Status : Detecting Object";
+objectinput = document.getElementById("Obj_input").value;
+}
+function modelloaded(){
+    console.log("modelloaded");
+    status1= true;
 }
